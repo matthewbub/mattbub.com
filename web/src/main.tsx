4356696845802ce919060c87ca700e3b2396bc25
matéo! <6matbub@gmail.com>
2025-08-02 23:53:07 -0700

    fix broken import

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import "./styles.css";
+import "./index.css";
 
 // Set up a Router instance
 const router = createRouter({
